6d2cf707aab5b0129148e5399faef332561edddb
dglazier <Derek.Glazier@glasgow.ac.uk>
2014-06-30 15:34:59 +0100

    Add project code for converting from g11 data to THSParticle  Changes to be committed: 	new file:   HaSpect/UnpackAnalysis.C 	new file:   Projects/BaseCLAStoHS/CLAStoHS.C 	new file:   Projects/BaseCLAStoHS/CLAStoHS.h 	new file:   Projects/BaseCLAStoHS/Control_CLAStoHS.C

---
+++ Projects/BaseCLAStoHS/Control_CLAStoHS.C
@@ -0,0 +1,21 @@
+{
+  //Set the input files
+   TChain* tree=new TChain("h10","datachain");
+   tree->Add("~dglazier/Work/Research/HaSpect/data/pippippimMn/inp*_50.root");
+  
+   //Set the ouput directory or file (if full file name is given just 1 file is created, while if a directory it will be created and filled with files of the same name as the input 
+   gSystem->Setenv("HSOUT",
+		   "/home/dglazier/Work/Research/HaSpect/data/pippippimMn_HS4/test.root");
+   
+   //Configure the analysis (i.e PROOF, source compilation...)
+   TString HSANA("/home/dglazier/Dropbox/HaSpect/Analysis/HaSpect/");
+   gSystem->Setenv("HSANA",HSANA);
+   gROOT->LoadMacro(HSANA+"HSControl.C");
+   HSControl(kFALSE); //where the proof stuff is set, kTRUE means use PROOF
+   
+   //Use an event list from a previous analysis 
+   //HSEntryList("~dglazier/Work/Research/HaSpect/data/pippippimMn_HS2/ParentEventList.root");
+   
+   //RUN
+   tree->Process("CLAStoHS.C++","pi+:pi+:pi-");
+}
