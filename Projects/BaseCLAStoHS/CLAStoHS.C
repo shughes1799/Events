c4b8d7bf0f327ad43c40f18a9a00dffe4be6dcb4
Simon Hughes <simon.hughes1799@gmail.com>
2014-12-05 15:41:14 +0000

    Added the histogramming functions along with TVectors for the pion combinations and a few additional variables for later use. Runs fine without proof, but causes a segmentation fault after the code has finished running if running with proof on.

--- Projects/BaseCLAStoHS/CLAStoHS.C
+++ Projects/BaseCLAStoHS/CLAStoHS.C
@@ -41,6 +41,16 @@ CLAStoHS::~CLAStoHS(){
 
   SafeDelete(fHSgamma);
   SafeDelete(fMissing);
+  SafeDelete(fOmegaCand1); //Added for fOmega Candidate 1
+  SafeDelete(fOmegaCand2); //Added for fOmega Candidate 2
+  SafeDelete(fOmegaCand3); //Added for fOmega Candidate 3
+  SafeDelete(fOmegaCand4); //Added for fOmega Candidate 4
+  SafeDelete(fPionComb1); //Added for fPionComb 1
+  SafeDelete(fPionComb2); //Added for fPionComb 2
+  SafeDelete(fPionComb3); //Added for fPionComb 3
+  SafeDelete(fPionComb4); //Added for fPionComb 4
+  SafeDelete(fBeamEnergySelection);
+
 }
 void CLAStoHS::Begin(TTree * /*tree*/)
 {
@@ -63,8 +73,61 @@ void CLAStoHS::SlaveBegin(TTree * /*tree*/)
    Int_t split=0;//note split is important in the TSelector framework, if increased branches in subsequent selectors will be data members of the THSParticle object rather than the whole object (this can lead to name conflicts)
    fHSgamma=new THSParticle(22); //beam
    fMissing=new TLorentzVector(); //missing
+   fOmegaCand1=new TLorentzVector(); //omega candidate1
+   fOmegaCand2=new TLorentzVector(); //omega candidate2
+   fOmegaCand3=new TLorentzVector(); //omega candidate3
+   fOmegaCand4=new TLorentzVector(); //omega candidate4
+   fPionComb1=new TLorentzVector(); //Other Pion Combination 1
+   fPionComb2=new TLorentzVector(); //Other Pion Combination 2
+   fPionComb3=new TLorentzVector(); //Other Pion Combination 3
+   fPionComb4=new TLorentzVector(); //Other Pion Combination 4
+   fBeamEnergySelection=new TLorentzVector();
+
+   //General Cut Variables
+   MissingMCLow = 0.105;
+   MissingMCHigh = 0.170;
+   OmegaMCLow = 0.65;
+   OmegaMCHigh = 0.92;
+   EtaMCLow = 0;
+   EtaMChigh = 0;
+
+   //Combination Parameters
+   double Par1[7] = {93577.99,-23139.8,43067.9,-23129.5,982.942,0.783338,0.0139013}; //Cand 1
+   double Par2[7] = {-16970.9,57122.7,-57869.2,17910.2,1116.62,0.782595,0.0117021};  //Cand 2
+   double Par3[7] = {-9192.09,25178.3,-15389.6,-440.733,1262.17,0.782384,0.0119449}; //Cand 3
+   double Par4[7] = {-38164.1,147453,-181252,72151.7,1207.61,0.781922,0.0109179};    //Cand 4
+
+   for (int i=0; i<7; i++) {
+     OmegaComb1Par[i] = Par1[i];
+     OmegaComb2Par[i] = Par2[i];
+     OmegaComb3Par[i] = Par3[i];
+     OmegaComb4Par[i] = Par4[i];
+   }
+
+   //Branches for the Output Tree
    fOutTree->Branch("beam",&fHSgamma,buff,split);
    fOutTree->Branch("miss",&fMissing,buff,split);
+   fOutTree->Branch("omega candidate1",&fOmegaCand1,buff,split);
+   fOutTree->Branch("omega candidate2",&fOmegaCand2,buff,split);
+   fOutTree->Branch("omega candidate3",&fOmegaCand3,buff,split);
+   fOutTree->Branch("omega candidate4",&fOmegaCand4,buff,split);
+   fOutTree->Branch("Other Pion Comb 1",&fPionComb1,buff,split);
+   fOutTree->Branch("Other Pion Comb 2",&fPionComb2,buff,split);
+   fOutTree->Branch("Other Pion Comb 3",&fPionComb3,buff,split);
+   fOutTree->Branch("Other Pion Comb 4",&fPionComb4,buff,split);
+   fOutTree->Branch("Beam Energy Selection",&fBeamEnergySelection,buff,split);
+
+
+   //Histogram Additions
+   //if you want kinematic bins you must define fHisbins here
+   //  fHisbins=new TH2F("My Bins","E and t",10,1.5,3.5,10,0,5);
+   if(fHisbins) fOutput->Add(fHisbins);
+   //  fHisbins->SetXTitle("Eg");//give useful axis name
+   //  fHisbins->SetYTitle("t");//give useful axis name
+   THSHisto::ChangeNames();
+   // THSHisto::LoadCut("mass_gt_0");
+   THSHisto::LoadCut("NoCut");
+   THSHisto::LoadHistograms();
   
 }
 
@@ -96,7 +159,29 @@ Bool_t CLAStoHS::Process(Long64_t entry)
    //example cut, missing mass must be within 50MeV of target mass
    // if(TMath::Abs(fMissing.M()-gTarget.M())>0.05) return kTRUE; //failed the cut, don't fill
    //all particles reconstructed, all cuts past, fill the output tree
+
+
+   //functions for determining Omega Candidates
+   *fOmegaCand1=*fMissing+fDetParticle[0]->P4()+fDetParticle[2]->P4();
+   *fOmegaCand2=*fMissing+fDetParticle[0]->P4()+fDetParticle[3]->P4();
+   *fOmegaCand3=*fMissing+fDetParticle[1]->P4()+fDetParticle[2]->P4();
+   *fOmegaCand4=*fMissing+fDetParticle[1]->P4()+fDetParticle[3]->P4();
+
+   //Functions for determining properties of different combinations of 2 detected pions particles [1]-[4]
+   *fPionComb1=fDetParticle[1]->P4()+fDetParticle[3]->P4();
+   *fPionComb2=fDetParticle[1]->P4()+fDetParticle[2]->P4();
+   *fPionComb3=fDetParticle[0]->P4()+fDetParticle[3]->P4();
+   *fPionComb4=fDetParticle[0]->P4()+fDetParticle[2]->P4();
  
+   //Histogram Additions
+   
+   //  Int_t kinBin=GetKinBin(beam->P4().E(),t);//if fHisbins is defined need to give this meaningful arguments
+   
+   FillHistograms("NoCut",0);
+   //FillHistograms("Cut1",kinBin);
+   //if(fDetParticle[0]->P4().M()>0) FillHistograms("mass_gt_0",0);
+   //if(beam->P4().E()>2) FillHistograms("Eg_gt_2",kinBin);
+   
    THSOutput::HSProcessFill();
    return kTRUE;
 }
@@ -209,7 +294,40 @@ Bool_t CLAStoHS::MakeBeam(Float_t Tmid,Float_t Tcut){
   if(!gotGamma) return kFALSE; //didn't get a photon event no good
   return kTRUE; //got one good photon
 }
+//Histogram Functions
+void CLAStoHS::HistogramList(TString sLabel){
+  TDirectory::AddDirectory(kFALSE); //do not add to current directory
+  //now define all histograms and add to Output
+  //label includes kinematic bin and additional cut name
+  // e.g fOutput->Add(MapHist(new TH1F("Mp1"+sLabel,"M_{p1}"+sLabel,100,0,2)));
+  
+  //Simon Histograms
+  fOutput->Add(MapHist(new TH2F("histo_n11"+sLabel,"Missing Mass vs #omega MassCand1;Missing Mass(GeV/c^{2});#omega Candidate Mass(GeV/c^{2})",50,0.05,0.20,50,0.4,1.0)));			    
+  fOutput->Add(MapHist(new TH2F("histo_n12"+sLabel,"Missing Mass vs #omega MassCand2;Missing Mass(GeV/c^{2});#omega Candidate Mass(GeV/c^{2})",300,-2,2,300,-2,2)));
+  fOutput->Add(MapHist(new TH2F("histo_n13"+sLabel,"Missing Mass vs #omega MassCand3;Missing Mass(GeV/c^{2});#omega Candidate Mass(GeV/c^{2})",50,0.05,0.20,50,0.4,1.0)));
+  fOutput->Add(MapHist(new TH2F("histo_n14"+sLabel,"Missing Mass vs #omega MassCand4;Missing Mass(GeV/c^{2});#omega Candidate Mass(GeV/c^{2})",50,0.05,0.20,50,0.4,1.0)));
 
+  //end of histogram list
+  TDirectory::AddDirectory(kTRUE); //back to normal
+}
+void CLAStoHS::FillHistograms(TString sCut,Int_t bin){
+  fCurrCut=sCut;
+  fCurrBin=bin;
+  //Get histogram from list
+  //Fill histogram
+  TString sLabel;
+  sLabel=sCut+fVecBinNames[bin];
+  // e.g. FindHist("Mp1"+sLabel)->Fill(fp1->M());
+
+  //Simon Histograms
+  //  FindHist("histo_n10"+sLabel)->Fill(fMissing->M(),OmegaMassTest);
+  FindHist("histo_n11"+sLabel)->Fill(fMissing->M(), fOmegaCand1->M());
+  FindHist("histo_n12"+sLabel)->Fill(fMissing->M(), fOmegaCand2->M());
+  FindHist("histo_n13"+sLabel)->Fill(fMissing->M(), fOmegaCand3->M());
+  FindHist("histo_n14"+sLabel)->Fill(fMissing->M(), fOmegaCand4->M());
+
+
+}
 void CLAStoHS::GetEventPartBranches(Int_t evi){
   b_cx->GetEvent(evi);
   b_cy->GetEvent(evi);
