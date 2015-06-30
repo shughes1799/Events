2044295be5798cf2ab19ffaee58d00d2362c4872
Simon Hughes <simon.hughes1799@gmail.com>
2015-06-30 16:25:01 +0100

    June 30th Restarting commits, initial commit

--- Projects/BaseCLAStoHS/CLAStoHS.h
+++ Projects/BaseCLAStoHS/CLAStoHS.h
@@ -56,6 +56,10 @@ class CLAStoHS : public TSelector, public  THSOutput {
 
  public :
   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+
+  //Histograms for the GetWeight Function
+  TF1* total;
+  TF1* g2;
   
   //Variables controlling the selection cuts on particles on interest for the different combinations of pions
   
@@ -109,10 +113,10 @@ class CLAStoHS : public TSelector, public  THSOutput {
   
   //Fitting Parameter Variables
 
-  Double_t OmegaComb1Par[7];
-  Double_t OmegaComb2Par[7];
-  Double_t OmegaComb3Par[7];
-  Double_t OmegaComb4Par[7];
+  Double_t OmegaComb1Par[8];
+  Double_t OmegaComb2Par[8];
+  Double_t OmegaComb3Par[8];
+  Double_t OmegaComb4Par[8];
   
   // Declaration of leaf types
   UShort_t        run_num;
@@ -506,7 +510,7 @@ class CLAStoHS : public TSelector, public  THSOutput {
   //fOutName - if full filename will just fill the single file with output of all input
 
   //Place to null pointers in the contructor
- CLAStoHS(TTree * /*tree*/ =0) : THSOutput(), fHSgamma(0), fMissing(0), fChain(0),fOmegaCand1(0), fOmegaCand2(0), fOmegaCand3(0), fOmegaCand4(0), fPionComb1(0), fPionComb2(0), fPionComb3(0), fPionComb4(0), fBeamEnergySelection(0), fProton(0) { }
+ CLAStoHS(TTree * /*tree*/ =0) : THSOutput(), fHSgamma(0), fMissing(0), fChain(0),fOmegaCand1(0), fOmegaCand2(0), fOmegaCand3(0), fOmegaCand4(0), fPionComb1(0), fPionComb2(0), fPionComb3(0), fPionComb4(0), fBeamEnergySelection(0), fProton(0), total(0), g2(0) { }
   //CLAStoHS(TTree * /*tree*/ =0) : THSOutput(), fHSgamma(0), fMissing(0), fChain(0) { fOutName="/home/dglazier/Work/Research/HaSpect/data/pippippimMn_HS2/OneFile.root";}
  
 
